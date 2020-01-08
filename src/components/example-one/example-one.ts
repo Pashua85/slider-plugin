c0481a5c424471f971727d8c1686ca1d60a2677e
Pavel Lashuk <pashua@Mac-mini-Pavel.local>
2020-01-08 14:07:49 +0500

    added form to example one

--- src/components/example-one/example-one.ts
+++ src/components/example-one/example-one.ts
@@ -5,16 +5,33 @@ const config1 = {
   minValue: 0,
   maxValue: 100,
   valueOne: 45,
-  valueTwo: 60,
-  // isVertical: true
+  valueTwo: 60
 };
 
 const slider1 = $('.slider-container--one').customSlider(config1);
-const outerInput1_1 = document.querySelector('#input-1-1');
-const outerInput1_2 = document.querySelector('#input-1-2');
+const outerInput1_1: HTMLInputElement = document.querySelector('#input-1-1');
+const outerInput1_2: HTMLInputElement = document.querySelector('#input-1-2');
+const button1: HTMLInputElement = document.querySelector('#radio-button-1-1');
+const button2: HTMLInputElement = document.querySelector('#radio-button-1-2');
 
 slider1.addOuterInputOne(outerInput1_1);
 slider1.addOuterInputTwo(outerInput1_2);
+
+button1.addEventListener('change', (event: Event) => {
+  if((<HTMLInputElement>event.target).checked) {
+    slider1.updateOptions ({
+      isVertical: false
+    })
+  };
+});
+
+button2.addEventListener('change', (event: Event) => {
+  if((<HTMLInputElement>event.target).checked) {
+    slider1.updateOptions ({
+      isVertical: true
+    })
+  };
+});
 
 
 
